39971c3e9d957e61b60eb59dd6ee158e61d3c4ee
Lucifer472 <JanvI@0427>
2024-04-12 16:21:29 +0530

    minor fix

--- app/(otherwrapper)/pancard-apply-process/page.tsx
+++ app/(otherwrapper)/pancard-apply-process/page.tsx
@@ -40,9 +40,7 @@ const PanCardApply = () => {
           </p>
         </div>
         <Link
-          href={
-            "/https://ojasinfo.in/pan-card-application-online-complete-information-2024"
-          }
+          href={"/pan-card-application-online-complete-information-2024"}
           className="max-w-7xl mx-auto w-full flex items-center justify-center my-1 px-2"
         >
           <Image
@@ -55,17 +53,13 @@ const PanCardApply = () => {
         <Ad2 />
         <div className="max-w-7xl mx-auto w-full flex items-center justify-center flex-col my-4 px-2 gap-y-2">
           <Link
-            href={
-              "/https://ojasinfo.in/pan-card-application-online-complete-information-2024"
-            }
+            href={"/pan-card-application-online-complete-information-2024"}
             className="w-full rounded-full bg-[#0072aa] text-2xl hover:bg-[#005eaa] text-white text-center py-4 md:py-6"
           >
             अनुरोध करना सीखें
           </Link>
           <Link
-            href={
-              "/https://ojasinfo.in/pan-card-application-online-complete-information-2024"
-            }
+            href={"/pan-card-application-online-complete-information-2024"}
             className="w-full rounded-full bg-[#0072aa] text-2xl hover:bg-[#005eaa] text-white text-center py-4 md:py-6"
           >
             ज्यादा जानें
