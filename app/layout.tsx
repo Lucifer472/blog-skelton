9c53447087c7622b97a1c64e66a90a747e9e915d
Lucifer472 <JanvI@0427>
2024-09-04 16:16:32 +0530

    added code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          async
+          strategy="afterInteractive"
+          id="google-ads"
+          crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2899879927145982"
+        />
         <Toaster position="top-center" />
         {children}
       </body>
